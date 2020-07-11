19a3c2cc9eb699b12856fb76a744d6568f5c52f0
Wesley Maa <wesley.maa@gmail.com>
2020-07-11 12:06:12 -0700

    added gSheets functionality

--- MACSniffer.py
+++ MACSniffer.py
@@ -1,7 +1,11 @@
 from scapy.all import ARP, Ether, srp
 import datetime
 import time
+import json
+import pygsheets
 
+
+print("Starting program")
 file = open("control", "w+")
 file.write("0")
 MACwhitelist = ["cc:40:d0:c2:5c:a2"]
@@ -9,14 +13,20 @@ MACTable = {}
 sessions = [] #Tuples of MAC, start, end
 
 run = True
+init = True
 while run:
+        if init:
+                print("Starting scans")
+                init = False
         time.sleep(5)
+        
         
         file.seek(0)
         control_flag = file.read().strip("\n")
         
         if control_flag != "0":
                 run = False
+                print("Shutting Down...")
 
         target_ip = "192.168.0.1/24"
         #IP Address for the destination
@@ -48,7 +58,7 @@ while run:
                         MACTable[mac] = [str(datetime.datetime.now())[11:19], 0]
         for mac in tempMacList:
                 if MACTable[mac][1] > 11:
-                        sessions.append((mac, MACTable.get(mac), str(datetime.datetime.now())[11:19]))
+                        sessions.append((mac, MACTable[mac][0], str(datetime.datetime.now())[11:19]))
                         del MACTable[mac]
                 else:
                         MACTable[mac][1] += 1
@@ -59,5 +69,35 @@ while run:
                 #print("{:16}	{}".format(client['ip'], client['mac']))
                 
 file.close()
-print(sessions)
-print(MACTable)
+tempData = {}
+with open("data.json", "r") as read_file:
+        tempData = json.load(read_file)
+
+lookup = {}
+with open("MACLookup.json", "r") as read_file:
+        lookup = json.load(read_file)
+
+data = MACTable.copy() 
+data["sessions"] = sessions
+data["lastRow"] = tempData.get("lastRow", 0)
+
+gc = pygsheets.authorize(service_file="gsCreds.json")
+
+currentRow = data["lastRow"]
+
+sheet = gc.open('Attendance')
+wks = sheet[0]
+
+for i in data["sessions"]:
+        letters = ["A", "B", "C"]
+        for x in range(3):
+                wks.update_value(letters[x] + str(currentRow), lookup.get(i[x], i[x]) if x == 0 else i[x])
+        wks.update_value("D{0}".format(str(currentRow)), '=C{0}-B{0}'.format(str(currentRow)), True)
+        wks.update_value("E{0}".format(str(currentRow)), '=index(split(D{0},":"),1)*60+index(split(D{0},":"),2)'.format(str(currentRow)), True)
+
+        currentRow += 1
+
+data["lastRow"] = currentRow
+
+with open("data.json", "w") as write_file:
+        json.dump(data, write_file)
